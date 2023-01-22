ea7c8e9dc24f325075b0991cd6e661f4165e4a6f
Volodymyr Kurov <vokur13@gmail.com>
2023-01-22 08:10:11 +0200

    validation scheme added

--- blogs/routes/posts.js
+++ blogs/routes/posts.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 
-const posts = [
+let posts = [
   { id: '1', title: 'test1', content: 'tex1t' },
   { id: '2', title: 'test2', content: 'text2' },
   { id: '3', title: 'test3', content: 'text3' },
@@ -20,19 +21,38 @@ router
   })
   /* GET api/posts/[id] listing. */
   .get('/:id', function (req, res, next) {
-    const [post] = posts.filter((item) => item.id === req.params.id);
-    res.json({
+    const { id } = req.params;
+    const [post] = posts.filter((item) => item.id === id);
+    if (!post) {
+      return res.status(404).json({
+        code: 404,
+        message: `Post id: ${id} not found`,
+      });
+    }
+    res.status(200).json({
       status: 'success',
       code: 200,
-      data: {
-        post,
-      },
+      data: { post },
     });
   })
   .post('/', (req, res) => {
+    const schema = Joi.object({
+      title: Joi.string().min(3).max(30).required(),
+      content: Joi.string().min(3).max(512).required(),
+    });
+
+    const validateData = schema.validate(req.body);
+
+    if (validateData.error) {
+      return res.status(404).json({
+        code: 404,
+        message: validateData.error.details,
+      });
+    }
+
     const { title, content } = req.body;
+
     posts.push({
-      //   id: Date.now().toString(),
       id: new Date().getTime().toString(),
       title,
       content,
@@ -43,10 +63,66 @@ router
       data: { posts },
     });
   })
-  .put('/:id', (req, res) => {})
+  .put('/:id', (req, res) => {
+    const schema = Joi.object({
+      title: Joi.string().min(3).max(30).required(),
+      content: Joi.string().min(3).max(512).required(),
+    });
+
+    const validateData = schema.validate(req.body);
+
+    if (validateData.error) {
+      return res.status(404).json({
+        code: 404,
+        message: validateData.error.details,
+      });
+    }
+
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const [post] = posts.filter((item) => item.id === id);
+    post.title = title;
+    post.content = content;
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { post },
+    });
+  })
+  .patch('/:id', (req, res) => {
+    const schema = Joi.object({
+      title: Joi.string().min(3).max(30).optional(),
+      content: Joi.string().min(3).max(512).optional(),
+    });
+
+    const validateData = schema.validate(req.body);
+
+    if (validateData.error) {
+      return res.status(404).json({
+        code: 404,
+        message: validateData.error.details,
+      });
+    }
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const [post] = posts.filter((item) => item.id === id);
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { post },
+    });
+  })
   .delete('/:id', (req, res) => {
-    const posts = posts.filter((item) => item.id !== req.params.id);
-    res.json({ posts });
+    const { id } = req.params;
+    const newPosts = posts.filter((item) => item.id !== id);
+    posts = [...newPosts];
+    res.status(204).json();
   });
 
 module.exports = { postsRouter: router };
